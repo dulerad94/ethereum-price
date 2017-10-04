ee52c3c10e4fe249c4ecd3f766df0169d0b611c9
Dusan <dulerad94@gmail.com>
2017-10-04 23:21:49 +0200

    all done

---
+++ Ethereum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Ethereum
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("Kernel32")]
+        private static extern IntPtr GetConsoleWindow();
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        static void Main(string[] args)
+        {
+            IntPtr hwnd;
+            hwnd = GetConsoleWindow();
+            ShowWindow(hwnd, SW_HIDE);
+
+            while (true)
+            {
+                try
+                {
+                    var request = (HttpWebRequest)WebRequest.Create("https://ethereumprice.org:443/wp-content/themes/theme/inc/exchanges/price-data.php?coin=eth&cur=ethusd&ex=waex&dec=2");
+
+                    request.Expect = "json";
+                    var response = (HttpWebResponse)request.GetResponse();
+
+                    var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    JObject obj = JObject.Parse(responseString);
+                    Response r = obj.ToObject<Response>();
+                    r.datetime = DateTime.Now;
+                    r.SaveToDatabase();
+                    Console.WriteLine(responseString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+                Thread.Sleep(300000);
+            }
+            
+        }
+       
+    }
+}
---
+++ Ethereum/Response.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ethereum
+{
+    class Response
+    {
+        public DateTime datetime { get; set; }
+        public string midnight_price { get; set; }
+        public string current_price { get; set; }
+        public string raw_price { get; set; }
+        public string percent_change { get; set; }
+        public string absolute_change { get; set; }
+        public string today_high { get; set; }
+        public string today_low { get; set; }
+        public string market_cap { get; set; }
+
+
+
+        public void SaveToDatabase()
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Dusan\\ethereum.mdf\";Integrated Security=True"))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;           
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT into response (datetime,midnight_price,current_price,raw_price,percent_change,absolute_change,"+
+                                        "today_high,today_low,market_cap) VALUES (@datetime,@midnight_price,@current_price,@raw_price,@percent_change,@absolute_change," +
+                                        "@today_high,@today_low,@market_cap)";
+                    command.Parameters.AddWithValue("@datetime", datetime);
+                    command.Parameters.AddWithValue("@midnight_price", midnight_price);
+                    command.Parameters.AddWithValue("@current_price", current_price);
+                    command.Parameters.AddWithValue("@raw_price", raw_price);
+                    command.Parameters.AddWithValue("@percent_change", percent_change);
+                    command.Parameters.AddWithValue("@absolute_change", absolute_change);
+                    command.Parameters.AddWithValue("@today_high", today_high);
+                    command.Parameters.AddWithValue("@today_low", today_low);
+                    command.Parameters.AddWithValue("@market_cap", market_cap);
+
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException e)
+                    {
+                        Console.WriteLine(e.StackTrace);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+    }
+}
